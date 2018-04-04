4e1493f0909aaf78b0069efebc64e71a34193ff8
Hasari90 <janus.hubert@wp.pl>
2018-04-04 16:38:33 +0200

    Start Unity project: Pacman Game

---
+++ Pacman/Assets/Script/PacmanMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacmanMove : MonoBehaviour {
+    public float speed = 0.4f;
+    Vector2 dest = Vector2.zero;
+
+    void Start()
+    {
+        dest = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 p = Vector2.MoveTowards(transform.position, dest, speed);
+        GetComponent<Rigidbody2D>().MovePosition(p);
+
+        if ((Vector2)transform.position == dest)
+        {
+            if (Input.GetKey(KeyCode.W) && Valid(Vector2.up))
+                dest = (Vector2)transform.position + Vector2.up;
+            if (Input.GetKey(KeyCode.D) && Valid(Vector2.right))
+                dest = (Vector2)transform.position + Vector2.right;
+            if (Input.GetKey(KeyCode.S) && Valid(-Vector2.up))
+                dest = (Vector2)transform.position - Vector2.up;
+            if (Input.GetKey(KeyCode.A) && Valid(-Vector2.right))
+                dest = (Vector2)transform.position - Vector2.right;
+        }
+
+        Vector2 dir = dest - (Vector2)transform.position;
+        GetComponent<Animator>().SetFloat("X", dir.x);
+        GetComponent<Animator>().SetFloat("Y", dir.y);
+    }
+
+    bool Valid(Vector2 dir)
+    {
+        Vector2 pos = transform.position;
+        RaycastHit2D hit = Physics2D.Linecast(pos + dir, pos);
+        return (hit.collider == GetComponent<Collider2D>());
+    }
+}
